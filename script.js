df299dc63fa68ccfe72f1be91ca6002af8e40364
Hussein Bahdon <35459369+H-BAHDON@users.noreply.github.com>
2023-05-16 01:45:17 -0700

    Updated script

--- script.js
+++ script.js
@@ -1,121 +1,122 @@
+import {handleSearch, getEpisodeCode, handleEpisodeSelection} from './helpers.js'
+
+
 function setup() {
-  const allEpisodes = getAllEpisodes();
-  makePageForEpisodes(allEpisodes);
+    const allEpisodes = getAllEpisodes();
+    makePageForEpisodes(allEpisodes);
 }
 
 function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
-  createHeader(rootElem)
+    const rootElem = document.getElementById("root");
+    createHeader(rootElem)
 
 
-  const mainElem = document.createElement("main");
-  mainElem.textContent = `Got ${episodeList.length} episode(s)`;
-  rootElem.appendChild(mainElem);
+    const mainElem = document.createElement("main");
+    rootElem.appendChild(mainElem);
 
-  createNav(rootElem)
-  allEpisodesDiv(rootElem, episodeList)
-  createFooter(rootElem)
+    createNav(rootElem, episodeList)
+    allEpisodesDiv(rootElem, episodeList)
+    createFooter(rootElem)
 }
 
 function allEpisodesDiv(parentElem, episodeList) {
-  const episodesContainer = document.createElement("div");
-  episodesContainer.classList.add("episode-Container-MainDiv");
+    const episodesContainer = document.createElement("div");
+    episodesContainer.classList.add("episode-Container-MainDiv");
 
-  episodeList.forEach((episode) => {
-    const episodeContainer = document.createElement("div");
-    episodeContainer.classList.add("episode-container");
+    episodeList.forEach((episode) => {
+        const episodeContainer = document.createElement("div");
+        episodeContainer.classList.add("episode-container");
 
-    const episodeCode = getEpisodeCode(episode.season, episode.number);
+        const episodeCode = getEpisodeCode(episode.season, episode.number);
 
-    const episodeCodeElem = document.createElement("p");
-    episodeCodeElem.textContent = episodeCode;
-    episodeContainer.appendChild(episodeCodeElem);
+        const episodeCodeElem = document.createElement("p");
+        episodeCodeElem.textContent = episodeCode;
+        episodeContainer.appendChild(episodeCodeElem);
 
-    const episodeNameElem = document.createElement("h2");
-    episodeNameElem.textContent = episode.name;
-    episodeContainer.appendChild(episodeNameElem);
+        const episodeNameElem = document.createElement("h2");
+        episodeNameElem.textContent = episode.name;
+        episodeContainer.appendChild(episodeNameElem);
 
-    const episodeImageElem = document.createElement("img");
-    episodeImageElem.src = episode.image.medium;
-    episodeImageElem.alt = episode.name;
-    episodeContainer.appendChild(episodeImageElem);
+        const episodeImageElem = document.createElement("img");
+        episodeImageElem.src = episode.image.medium;
+        episodeImageElem.alt = episode.name;
+        episodeContainer.appendChild(episodeImageElem);
 
-    const episodeSummaryElem = document.createElement("p");
-    episodeSummaryElem.innerHTML = episode.summary;
-    episodeContainer.appendChild(episodeSummaryElem);
+        const episodeSummaryElem = document.createElement("p");
+        episodeSummaryElem.innerHTML = episode.summary;
+        episodeContainer.appendChild(episodeSummaryElem);
 
-    const episodeLinkElem = document.createElement("a");
-    episodeLinkElem.href = episode.url;
-    episodeLinkElem.textContent = "More info on TVMaze.com";
-    episodeContainer.appendChild(episodeLinkElem);
+        const episodeLinkElem = document.createElement("a");
+        episodeLinkElem.href = episode.url;
+        episodeLinkElem.textContent = "More info on TVMaze.com";
+        episodeContainer.appendChild(episodeLinkElem);
 
-    episodesContainer.appendChild(episodeContainer);
-  });
+        episodesContainer.appendChild(episodeContainer);
+    });
 
-  parentElem.appendChild(episodesContainer);
+    parentElem.appendChild(episodesContainer);
 }
 
-function getEpisodeCode(season, episode) {
-  const seasonCode = String(season).padStart(2, "0");
-  const episodeCode = String(episode).padStart(2, "0");
-  return `S${seasonCode}E${episodeCode}`;
+function createHeader(parentElem) {
+    const headerElem = document.createElement("header");
+    headerElem.classList.add("banner")
+    headerElem.textContent = "My Episode List";
+    parentElem.appendChild(headerElem);
+}
+
+function createNav(parentElem, episodeList) {
+    const navElem = document.createElement("nav");
+    navElem.classList.add("nav-Banner");
+
+    // Create the selector section
+    const selectorDiv = document.createElement("div");
+    selectorDiv.classList.add("selector");
+
+    // search bar
+    const searchInput = document.createElement("input");
+    searchInput.type = "text";
+    searchInput.placeholder = "Search episodes";
+    searchInput.addEventListener("input", handleSearch);
+    selectorDiv.appendChild(searchInput);
+    
+
+    const selectInput = document.createElement("select");
+    selectInput.addEventListener("change", handleEpisodeSelection);
+    selectorDiv.appendChild(selectInput);
+
+    const defaultOption = document.createElement("option");
+    defaultOption.textContent = "Jump to an episode";
+    selectInput.appendChild(defaultOption);
+
+    episodeList.forEach((episode, index) => {
+        const option = document.createElement("option");
+        const episodeCode = getEpisodeCode(episode.season, episode.number);
+        option.textContent = `${episodeCode} - ${
+            episode.name
+        }`;
+        option.value = index;
+        selectInput.appendChild(option);
+    });
+
+    // Show "Show all episodes" button
+    const showAllButton = document.createElement("button");
+    showAllButton.textContent = "Show all episodes";
+    showAllButton.addEventListener("click", () => {
+        document.querySelectorAll(".episode-container").forEach((container) => {
+            container.style.display = "block";
+        });
+    });
+    selectorDiv.appendChild(showAllButton);
+
+    navElem.appendChild(selectorDiv);
+    parentElem.appendChild(navElem);
 }
 
 
-function createHeader(parentElem){
-  const headerElem = document.createElement("header");
-  headerElem.classList.add("banner")
-  headerElem.textContent = "My Episode List"; // Replace with your desired header content
-  parentElem.appendChild(headerElem);
-}
-
-function createNav(parentElem) {
-  const navElem = document.createElement("nav");
-  navElem.classList.add("nav-Banner");
-
-  // Create the selector section
-  const selectorDiv = document.createElement("div");
-  selectorDiv.classList.add("selector");
-
-  // Create the select button
-  const selectButton = document.createElement("select");
-  selectButton.textContent = "Select a Show";
-
-  const seasonButton = document.createElement("select");
-  seasonButton.textContent = "Select a season";
-
-
-  // Create the default option
-  const defaultOptionForSelect = document.createElement("option");
-  defaultOptionForSelect.textContent = "Choose a show";
-  selectButton.appendChild(defaultOptionForSelect);
-
-  const defaultOption = document.createElement("option");
-  defaultOption.textContent = "Choose a season";
-  seasonButton.appendChild(defaultOption);
-  
-  // Create the show options
-  // const show1Option = document.createElement("option");
-  // show1Option.textContent = "Show 1";
-  // selectButton.appendChild(show1Option)
-
-
-
-  // Append the select button to the selector section
-  selectorDiv.appendChild(selectButton);
-  selectorDiv.appendChild(seasonButton);
-  // Append the selector section to the navigation
-  navElem.appendChild(selectorDiv);
-
-  // Append the navigation to the parent element
-  parentElem.appendChild(navElem);
-}
-
-
-function createFooter(parentElem){
-  const footerElem = document.createElement("footer");
-  footerElem.textContent = "© 2023 My Website"; // Replace with your desired footer content
-  parentElem.appendChild(footerElem);
+function createFooter(parentElem) {
+    const footerElem = document.createElement("footer");
+    footerElem.textContent = " 2023 My Website";
+    parentElem.appendChild(footerElem);
 }
 
 window.onload = setup;
